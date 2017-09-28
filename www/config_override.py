566f87425dc348d00e340b69300838e19c09758d
frank1in <wuhj9608@gmail.com>
2017-09-28 14:39:57 +0800

    第一次提交，基本照搬廖雪峰老师的代码。

---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+configs = {
+    'db': {
+        'host': '10.0.0.1',
+        'user': 'frank1in',
+        'password': 'sqlpasswd',
+        'db': 'web_app'
+    },
+    'session': {
+        'secreat': 'frank1in'
+    }
+}
